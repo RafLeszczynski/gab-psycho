bf3ef07511f4eff62bc89aadb63ba62104d84cb3
Rafal Leszczynski <rafal@wikia-inc.com>
2014-02-09 16:41:31 +0100

    favicon + image optimizations + IE nav bug

--- site.js
+++ site.js
@@ -4,10 +4,12 @@ var connect = require( 'connect' ),
 	http = require( 'http'),
 
 	port = Number( process.env.PORT || 5000 ),
-	root = '/public';
+	root = '/public',
+	faviconPath = '/assets/images/favicon.ico';
 
 http.createServer(
 	connect()
+		.use( connect.favicon( __dirname + root + faviconPath ) )
 		.use( connect.logger() )
 		.use( connect.compress() )
 		.use( connect.static( __dirname + root ) ),
